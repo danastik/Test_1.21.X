dc6c85f9335f00937bea453ad3b8f8a6e6f43d4c
danastik <stoikovdaniilan@gmail.com>
2026-01-27 23:01:43 +0300

    Initial commit

---
+++ src/main/java/net/danastik/test/TestClient.java
@@ -0,0 +1,10 @@
+package net.danastik.test;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/danastik/test/Test_121XDataGenerator.java
@@ -0,0 +1,11 @@
+package net.danastik.test;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Test_121XDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
