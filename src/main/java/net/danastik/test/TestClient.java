44a60b30db7b1bfd3b01b0c87d937c2e48d55ddf
danastik <stoikovdaniilan@gmail.com>
2026-01-28 12:33:25 +0300

    added possibility to craft cheese and added texture :з

--- src/main/java/net/danastik/test/TestClient.java
+++ src/main/java/net/danastik/test/TestClient.java
@@ -1,10 +1,11 @@
 package net.danastik.test;
 
+import net.danastik.test.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TestClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/net/danastik/test/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.danastik.test.item;
+
+import net.danastik.test.Test_121X;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item CHEESE = registerItem("cheese", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Test_121X.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Test_121X.LOGGER.info("Registering Mod Items for " + Test_121X.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(CHEESE);
+        });
+    }
+}
